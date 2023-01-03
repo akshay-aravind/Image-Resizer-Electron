a13e17dc488042795d3ddc60601af069f412d0b8
Akshay Aravind <akshay.dx4@gmail.com>
2023-01-03 12:25:08 +0530

    isMac err and devtools

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const path = require("path")
+const { app, BrowserWindow } = require("electron")
+
+const isDev = process.env.NODE_ENV !== "production"
+const isMac = process.platform === "darwin"
 
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
-        title: 'Image Resizer',
-        width: 500,
-        height: 600
-    })
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: isDev ? 1000 : 500,
+    height: 600,
+  })
 
-    mainWindow.loadFile(path.join(__dirname,'./renderer/index.html'));
+  //Open devtools if in dev env
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"))
 }
 
 app.whenReady().then(() => {
-   app.on('activate', () => {
+ createMainWindow()
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
   })
 })
 
-
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) app.quit()
 })
