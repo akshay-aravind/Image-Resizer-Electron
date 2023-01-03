c5f52425414f813204aa55ebf7752efb289f4242
Akshay Aravind <akshay.dx4@gmail.com>
2023-01-03 12:02:11 +0530

    boilerplate

---
+++ main.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: 500,
+        height: 600
+    })
+
+    mainWindow.loadFile(path.join(__dirname,'./renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+   app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+  })
+})
+
+
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit()
+})
